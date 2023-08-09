e40a683c65be63684a8773b82baf31840ccdac37
Aligg <114702742+henriqueamorimm@users.noreply.github.com>
2023-08-08 23:50:15 -0300

    Create main.php

---
+++ main.php
@@ -0,0 +1,19 @@
+<?php
+function generateDUMBO() {
+    if (function_exists('com_create_guid') === true) {
+        return trim(com_create_guid(), '{}');
+    }
+    
+    return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+        mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+        mt_rand(0, 0xffff),
+        mt_rand(0, 0x0fff) | 0x4000,
+        mt_rand(0, 0x3fff) | 0x8000,
+        mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+    );
+}
+
+$uuid = generateDUMBO();
+echo $uuid;
+
+?>
